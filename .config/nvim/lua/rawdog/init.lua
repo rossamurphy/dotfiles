eddb1e57102a3f2f929c21cd993b61783b0ac1f3
rossamurphy <ross.a.murphy@gmail.com>
2024-06-10 23:44:50 +0100

    direct path

--- .config/nvim/lua/rawdog/init.lua
+++ .config/nvim/lua/rawdog/init.lua
@@ -1,4 +1,4 @@
-require('rawdog.remap')
-require('rawdog.set')
--- require('/root/.config/nvim/lua/rawdog/remap')
--- require('/root/.config/nvim/lua/rawdog/set')
+-- require('rawdog.remap')
+-- require('rawdog.set')
+require('/root/.config/nvim/lua/rawdog/remap')
+require('/root/.config/nvim/lua/rawdog/set')
