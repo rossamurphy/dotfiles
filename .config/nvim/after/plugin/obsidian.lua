a1e1df66ddf92f0e21069c9d3d580e2000840692
rossamurphy <ross.a.murphy@gmail.com>
2025-07-22 10:09:14 +0100

    fix

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -29,7 +29,7 @@ require("obsidian").setup({
 
 	daily_notes = {
 		-- Optional, if you keep daily notes in a separate directory.
-		folder = "notes/dailies",
+		folder = "research/notes/dailies",
 		-- Optional, if you want to change the date format for the ID of daily notes.
 		date_format = "%Y-%m-%d",
 		-- Optional, if you want to change the date format of the default alias of daily notes.
