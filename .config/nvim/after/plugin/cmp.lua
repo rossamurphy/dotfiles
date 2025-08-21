f498b0212a864cf9c1694ad143dc7ff4ae6c335c
rossamurphy <ross.a.murphy@gmail.com>
2025-08-21 22:58:34 +0100

    ronan carriga, thank you.

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -1,39 +1,51 @@
 return {
-  "hrsh7th/nvim-cmp",
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	---@param opts cmp.ConfigSchema
+	opts = function(_, opts)
+		require("cmp").setup({
+			enabled = function()
+				return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+			end,
+		})
 
-    -- The `mapping` table below will be merged with LazyVim's defaults.
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          -- If the completion window is visible, select the next item
-          cmp.select_next_item()
-        else
-          -- Otherwise, fall back to the default Tab behavior (like indentation)
-          fallback()
-        end
-      end, { "i", "s" }),
+		require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+			sources = {
+				{ name = "dap" },
+			},
+		})
 
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          -- If the completion window is visible, select the previous item
-          cmp.select_prev_item()
-        else
-          -- Otherwise, fall back to the default Shift-Tab behavior
-          fallback()
-        end
-      end, { "i", "s" }),
+		local cmp = require("cmp")
 
-      -- Use <Space> to confirm the selected completion
-      [" "] = cmp.mapping.confirm({ select = true }),
-    })
+		-- The `mapping` table below will be merged with LazyVim's defaults.
+		opts.mapping = vim.tbl_extend("force", opts.mapping, {
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					-- If the completion window is visible, select the next item
+					cmp.select_next_item()
+				else
+					-- Otherwise, fall back to the default Tab behavior (like indentation)
+					fallback()
+				end
+			end, { "i", "s" }),
 
-    -- NOTE: By default, <CR> (Enter) also confirms completion.
-    -- If you want ONLY <Space> to confirm, uncomment the line below:
-    -- opts.mapping["<CR>"] = nil
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					-- If the completion window is visible, select the previous item
+					cmp.select_prev_item()
+				else
+					-- Otherwise, fall back to the default Shift-Tab behavior
+					fallback()
+				end
+			end, { "i", "s" }),
 
-    return opts
-  end,
+			-- Use <Space> to confirm the selected completion
+			[" "] = cmp.mapping.confirm({ select = true }),
+		})
+
+		-- NOTE: By default, <CR> (Enter) also confirms completion.
+		-- If you want ONLY <Space> to confirm, uncomment the line below:
+		-- opts.mapping["<CR>"] = nil
+
+		return opts
+	end,
 }
