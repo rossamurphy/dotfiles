6ac8b87a976dab94845fa621973b30ff0ac38854
rossamurphy <ross.a.murphy@gmail.com>
2026-02-25 10:35:01 +0000

    maybe necessary

--- .config/nvim/after/plugin/markdown-preview.lua
+++ .config/nvim/after/plugin/markdown-preview.lua
@@ -2,6 +2,7 @@
 -- https://github.com/iamcco/markdown-preview.nvim
 
 vim.g.mkdp_port = '9999'
+vim.g.mkdp_open_to_the_world = 1
 
 -- Set up keymap to toggle markdown preview
 vim.keymap.set("n", "<leader>im", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
